3e2ea846bbecd6cf5408afda81d35769bbc7378f
Erik Zephyr <erikbv99421>
2018-11-06 15:50:11 +0000

    updating readme

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi humans! bye
+            Hi humans! byee
           </p>
           <a
             className="App-link"
